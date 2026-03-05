7b9046124586c85316a0f2ccf05b9ea1b2e755c6
erichategekimana <h.eric@alustudent.com>
2026-03-05 10:50:05 +0200

    bug fix

---
+++ backend_fromework/config.py
@@ -0,0 +1,11 @@
+import os
+from datetime import timedelta
+
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    JWT_SECRET = os.environ.get("JWT_SECRET")
+    JWT_ALGORITH = "HS256"
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
+    SQLALCHEMY_DATABASE_URI = os.environ.get("BP_SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_TRACK_MODIFICATIONS =False
